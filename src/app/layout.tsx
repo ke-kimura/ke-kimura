5f2ea7e173164cf93889e3fbbc0a1c012700fe20
ke-kimura <kimura7298@yahoo.co.jp>
2024-04-04 21:57:34 +0900

    add-metadata-interface

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,13 @@ import './globals.css';
 import { Inter } from 'next/font/google';
 import NextAuthProvider from '@/providers/NextAuth';
 
+// Metadata型の定義
+interface Metadata {
+  title: string;
+  description: string;
+}
+
+
 const inter = Inter({ subsets: ['latin'] });
 
 export const metadata: Metadata = {
